09a651b4d47d1792d29027423dc98107fbe9eea7
kalinavladd <kalinavladd@gmail.com>
2023-02-06 13:03:57 +0200

    first commit

--- src/leetcode_easy/length_of_last_word.rs
+++ src/leetcode_easy/length_of_last_word.rs
@@ -9,3 +9,10 @@ pub fn length_of_last_word(s: String) -> i32 {
     };
     count
 }
+
+#[test]
+fn test_length_of_last_word() {
+    assert_eq!(length_of_last_word(String::from("Hello World")), 5);
+    assert_eq!(length_of_last_word(String::from("   fly me   to   the moon  ")), 4);
+    assert_eq!(length_of_last_word(String::from("luffy is still joyboy")), 6);
+}
