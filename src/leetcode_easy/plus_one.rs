9ad6703c2b44c1d853ff26d0556493c141715523
kalinavladd <kalinavladd@gmail.com>
2023-02-06 15:19:27 +0200

    plu one

--- src/leetcode_easy/plus_one.rs
+++ src/leetcode_easy/plus_one.rs
@@ -1,17 +1,15 @@
-
-pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-    let mut str_digits = String::new();
-
-    for num in digits {
-        str_digits += &*num.to_string();
+pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+    for num in digits.iter_mut().rev() {
+        match *num == 9 {
+            true => *num = 0,
+            false => {
+                *num += 1;
+                return digits;
+            }
+        }
     }
-    let result = str_digits.parse::<i32>().unwrap() + 1;
-    let mut result_digits: Vec<i32> = Vec::new();
-    for i in result.to_string().chars() {
-        result_digits.push(i.to_digit(10).unwrap() as i32);
-    };
-    result_digits
-
+    digits.insert(0, 1);
+    digits
 }
 
 
@@ -20,4 +18,6 @@ fn test_plus_one() {
     assert_eq!(plus_one(vec![1, 2, 3]), vec![1, 2, 4]);
     assert_eq!(plus_one(vec![4, 3, 2, 1]), vec![4, 3, 2, 2]);
     assert_eq!(plus_one(vec![9]), vec![1, 0]);
+    assert_eq!(plus_one(vec![9,8,7,6,5,4,3,2,1,0]), vec![9,8,7,6,5,4,3,2,1,1]);
+    assert_eq!(plus_one(vec![1, 2, 3, 4, 5, 6, 7, 8, 9]), vec![1, 2, 3, 4, 5, 6, 7, 9, 0]);
 }
